74fbb3d643995b73e54d13f3f0fcd5a084e86fd8
Dennis Chung <chungdennis6@gmail.com>
2019-11-18 03:32:47 +0000

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,34 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+/**
+ * Time passed in in format HH:MM
+ * e.g.
+ * 05:33
+ * 12:45
+ * 18:23
+ * @param {*} time 
+ */
+function greet(time) {
+  let x = time.split(':');
+  let hours = parseInt(x[0], 10);
+  if (hours < 12) {
+    return "Good Morning";
+  }
+  else if (hours < 17) {
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening";
+  }
+}
+
+
+function displayMessage(update) {
+  const greeting = document.getElementById('greeting');
+  if (greeting) {
+    greeting.innerText = update;
+  }
+
+}
